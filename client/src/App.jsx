a1d6620d2529deb8483d229ac9098ff1b4675d60
Bhusan Dotel <bhusandotel1@gmail.com>
2025-02-24 12:21:35 +0545

    Add CORS support and integrate socket.io in client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,36 @@
+import React from "react";
+import axios from "axios";
+import io from "socket.io-client";
+const host = "http://localhost:5000";
+
+const socket = io.connect(host);
+
 const App = () => {
-  return <div>This is app </div>;
+  const hitApi = async () => {
+    const response = await axios.post(`${host}/api/socket-test`, {
+      name: "Test",
+    });
+
+    console.log(response.data);
+  };
+
+  const doLogics = React.useCallback((message) => {
+    alert(message);
+  }, []);
+
+  React.useEffect(() => {
+    socket.on("testEvent", (testEvent) => {
+      doLogics(testEvent.message);
+    });
+  }, [doLogics]);
+
+  return (
+    <div>
+      <button className="bg-red-500 cursor-pointer" onClick={hitApi}>
+        Hit API
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 import { connectDB } from "./db.js";
 import bodyParser from "body-parser";
 import http from "http";
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 await connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,5 +29,5 @@ const server = http.createServer(app);
 initSocket(server);
 
 server.listen(PORT, () => {
-  console.log("App listening on port 3000!");
+  console.log(`App listening on port ${PORT}`);
 });
